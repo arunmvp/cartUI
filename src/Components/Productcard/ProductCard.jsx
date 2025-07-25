3826268ccb7675bbf48b46776f37e7df7d0af205
arunmvp <mvparun3@gmail.com>
2025-07-25 21:17:35 +0530

    updated

--- src/Components/Productcard/ProductCard.jsx
+++ src/Components/Productcard/ProductCard.jsx
@@ -1,37 +1,55 @@
-import React from 'react';
-import './ProductCard.css';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addItem } from '../../redux/CreateSlice';
+import React, { useState } from "react";
+import "./ProductCard.css";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../redux/CreateSlice";
 
-
-const ProductCard = ({id, title, price, images,thumbnail,description,rating }) => {
-
+const ProductCard = ({
+  id,
+  title,
+  price,
+  images,
+  thumbnail,
+  description,
+  rating,
+}) => {
   const [imageset, setImageset] = useState(false);
   const [load, setLoad] = useState(false);
 
   const dispatch = useDispatch();
 
-  const setLoading = () => { 
-    setLoad(true); 
+  const setLoading = () => {
+    setLoad(true);
     setTimeout(() => {
       setLoad(false);
-    }, 1500)
-  }
-  
-    
+    }, 1500);
+  };
 
   return (
     <div className="product-card">
-      <img src={imageset ? thumbnail : images[0]} alt={title} onMouseEnter={()=>{setImageset(true)}} onMouseLeave={()=>{setImageset(false)}} /> 
+      <img
+        src={imageset ? thumbnail : images[0]}
+        alt={title}
+        onMouseEnter={() => {
+          setImageset(true);
+        }}
+        onMouseLeave={() => {
+          setImageset(false);
+        }}
+      />
       <h3>{title}</h3>
-      <p className='des'>{description}</p> 
-      <p className='des1'>Rating : {rating}</p> 
+      <p className="des">{description}</p>
+      <p className="des1">Rating : {rating}</p>
       <p>₹{price}</p>
-      <button onClick={()=> { dispatch(addItem({id,title,price,images})) , setLoading()}}>{!load ? "Add to cart" : <span className='spinner'></span>}</button> 
-      
+      <button
+        onClick={() => {
+          dispatch(addItem({ id, title, price, images }));
+          setLoading();
+        }}
+      >
+        {!load ? "Add to cart" : <span className="spinner"></span>}
+      </button>
     </div>
   );
 };
 
-export default ProductCard
+export default ProductCard;
